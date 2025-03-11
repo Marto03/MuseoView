6d4674eb550236b0555cd1722911e8a043041156
Marto03 <marti775@abv.bg>
2025-03-11 10:20:08 +0200

    Creating the project

---
+++ MuseoView/MuseoView/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MuseoView;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
