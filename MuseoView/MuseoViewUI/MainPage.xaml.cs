ea9eb738f8c2938359a1bf8a41a12ac1c0a20e4e
Marto03 <marti775@abv.bg>
2025-04-19 12:04:59 +0300

    added 360 image with gyroscope functionality

--- MuseoView/MuseoViewUI/MainPage.xaml.cs
+++ MuseoView/MuseoViewUI/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using MuseoViewUI.ViewModels;
+﻿using Microsoft.Maui.Handlers;
+using MuseoViewUI.ViewModels;
 
 namespace MuseoViewUI
 {
@@ -28,6 +29,36 @@ namespace MuseoViewUI
         private void OnSearchClicked(object sender, EventArgs e)
         {
 
+        }
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            // Enable JS and local file access
+#if ANDROID
+            WebViewHandler.Mapper.AppendToMapping("EnableJavaScript", (handler, view) =>
+            {
+                handler.PlatformView.Settings.JavaScriptEnabled = true;
+                handler.PlatformView.Settings.DomStorageEnabled = true;
+                handler.PlatformView.Settings.AllowFileAccess = true;
+                handler.PlatformView.Settings.AllowFileAccessFromFileURLs = true;
+                handler.PlatformView.Settings.AllowUniversalAccessFromFileURLs = true;
+            });
+
+            //if (PanoramaWebView.Handler?.PlatformView is Android.Webkit.WebView nativeWebView)
+            //{
+            //    nativeWebView.SetOnTouchListener(new WebViewTouchListener());
+
+            //    // Включи gyroscope само при фокус
+            //    var activity = Platform.CurrentActivity as MainActivity;
+            //    activity?.RegisterGyro(nativeWebView);
+            //}
+#endif
+                    // Load the local HTML (bundled via MauiAsset)
+                    //PanoramaWebView.Source = new UrlWebViewSource
+                    //{
+                    //    Url = "file:///android_asset/viewer.html"
+                    //};
         }
 
         //private void OnRemainingItemsThresholdReached(object sender, EventArgs e)
--- MuseoView/MuseoViewUI/Platforms/Android/MainActivity.cs
+++ MuseoView/MuseoViewUI/Platforms/Android/MainActivity.cs
@@ -1,10 +1,57 @@
 ﻿using Android.App;
 using Android.Content.PM;
+using Android.Hardware;
 using Android.OS;
+using Android.Webkit;
+using AWebView = Android.Webkit.WebView;
 
 namespace MuseoViewUI;
 
 [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
-public class MainActivity : MauiAppCompatActivity
+public class MainActivity : MauiAppCompatActivity, ISensorEventListener
 {
+    SensorManager _sensorManager;
+    Sensor? _rotationSensor;
+    AWebView? _webView;
+
+    protected override void OnCreate(Android.OS.Bundle? savedInstanceState)
+    {
+        base.OnCreate(savedInstanceState);
+
+        _sensorManager = (SensorManager)GetSystemService(SensorService)!;
+        _rotationSensor = _sensorManager.GetDefaultSensor(SensorType.RotationVector);
+    }
+
+    public void RegisterGyro(AWebView webView)
+    {
+        _webView = webView;
+        _sensorManager.RegisterListener(this, _rotationSensor, SensorDelay.Game);
+    }
+
+    public void UnregisterGyro()
+    {
+        _sensorManager.UnregisterListener(this);
+        _webView = null;
+    }
+
+    public void OnSensorChanged(SensorEvent e)
+    {
+        if (e.Sensor.Type != SensorType.RotationVector || _webView == null)
+            return;
+
+        float[] rotationMatrix = new float[9];
+        SensorManager.GetRotationMatrixFromVector(rotationMatrix, e.Values.ToArray());
+
+        float[] orientation = new float[3];
+        SensorManager.GetOrientation(rotationMatrix, orientation);
+
+        float azimuth = orientation[0]; // Завъртане по Z
+        float pitch = orientation[1];   // Наклон
+        float roll = orientation[2];    // Странично завъртане
+
+        string js = $"rotateView({azimuth}, {pitch}, {roll});";
+        _webView.Post(() => _webView.EvaluateJavascript(js, null));
+    }
+
+    public void OnAccuracyChanged(Sensor sensor, SensorStatus accuracy) { }
 }
